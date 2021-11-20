3c071e44929dc4f81a61a996accf4286f7491469
Brian Park <brianshockerpark@gmail.com>
2021-11-20 15:38:28 -0800

    Update - 11/20/2021

--- etch.js
+++ etch.js
@@ -9,118 +9,56 @@ const Save_button=document.getElementById("save");
 const Slider=document.getElementById("slider");
 const Dimension=document.getElementById("dimension");
 const Box=document.getElementById("box");
-
 const Default=16;
+const nameArray= [Draw_button,Eraser_button,Rainbow_button,Fill_button,Color_button];
 let x=0;
-let y=0;
 
-function Draw() {
-    x++;
-    if(x % 2 === 0){
-        Draw_button.style.backgroundColor= "#FE3540";
-        Eraser_button.disabled = false;
-        Rainbow_button.disabled = false;
-        Fill_button.disabled = false;
-        Color_button.disabled = false;
-        x=0; 
-    } 
-    else{
-        Draw_button.style.backgroundColor="darkred";
-        Eraser_button.disabled = true;
-        Rainbow_button.disabled = true;
-        Fill_button.disabled = true;
-        Color_button.disabled = true;
+Draw_button.addEventListener("click", function() {
+    for (let i = 0; i <= 4 ; i++) {
+        nameArray[i].style.backgroundColor="#FE3540";
     }
-}
+    Draw_button.style.backgroundColor="darkred";
+}); 
+Eraser_button.addEventListener("click", function() {
+    for (let i = 0; i <= 4 ; i++) {
+        nameArray[i].style.backgroundColor="#FE3540";
+    }
+    Eraser_button.style.backgroundColor="darkred";
+}); 
+Rainbow_button.addEventListener("click", function() {
 
-function Eraser() {
-    x++;
-    if(x % 2 === 0){
-        Eraser_button.style.backgroundColor="#FE3540";
-        Draw_button.disabled = false;
-        Rainbow_button.disabled = false;
-        Fill_button.disabled = false;
-        Color_button.disabled = false;
-        x=0;
-    } 
-    else{
-        Eraser_button.style.backgroundColor="darkred";
-        Rainbow_button.disabled = true;
-        Fill_button.disabled = true;
-        Color_button.disabled = true;
-        Draw_button.disabled = true;
+    for (let i = 0; i <= 4 ; i++) {
+        nameArray[i].style.backgroundColor="#FE3540";
     }
-}
-function Rainbow() {
-    x++;
-    if(x % 2 === 0){
-        Rainbow_button.style.backgroundColor="#FE3540";
-        Eraser_button.disabled = false;
-        Draw_button.disabled = false;
-        Fill_button.disabled = false;
-        Color_button.disabled = false;
-        x=0;
-        
-    } 
-    else{
-        Rainbow_button.style.backgroundColor="darkred";
-        Eraser_button.disabled = true;
-        Fill_button.disabled = true;
-        Color_button.disabled = true;
-        Draw_button.disabled = true;
+    Rainbow_button.style.backgroundColor="darkred";
+}); 
+Fill_button.addEventListener("click", function() {
+    for (let i = 0; i <= 4 ; i++) {
+        nameArray[i].style.backgroundColor="#FE3540";
     }
-}
-function Fill() {
-    x++;
-    if(x % 2 === 0){
-        Fill_button.style.backgroundColor="#FE3540";
-        Eraser_button.disabled = false;
-        Rainbow_button.disabled = false;
-        Draw_button.disabled = false;
-        Color_button.disabled = false;
-    } 
-    else{
-        Fill_button.style.backgroundColor="darkred";
-        Eraser_button.disabled = true;
-        Rainbow_button.disabled = true;
-        Color_button.disabled = true;
-        Draw_button.disabled = true;
+    Fill_button.style.backgroundColor="darkred";
+}); 
+Color_button.addEventListener("click", function() {
+    for (let i = 0; i <= 4 ; i++) {
+        nameArray[i].style.backgroundColor="#FE3540";
     }
-}
-function Color() {
-    x++;
-    if(x % 2 === 0){
-        Color_button.style.backgroundColor="#FE3540";
-        Eraser_button.disabled = false;
-        Rainbow_button.disabled = false;
-        Fill_button.disabled = false;
-        Draw_button.disabled = false;
-    } 
-    else{
-        Color_button.style.backgroundColor="darkred";
-        Eraser_button.disabled = true;
-        Fill_button.disabled = true;
-        Rainbow_button.disabled = true;
-        Draw_button.disabled = true;
-    }
-}
+    Color_button.style.backgroundColor="darkred";
+});
+
+Clear_button.addEventListener("click", function() {
+    Clear_button.style.backgroundColor="darkred";
+    animate();
+    setTimeout(() => {
+        Clear_button.style.backgroundColor="#FE3540";
+        Box.style.animation = "reset";
+    }, 1000); 
+}); 
 
 
-function Clear() {
-    y++;
-    if(y === 1){
-        Clear_button.style.backgroundColor="darkred";
-        animate();
-        setTimeout(() => {
-            Clear_button.style.backgroundColor="#FE3540";
-            Box.style.animation = "reset";
-        }, 1000);
-        y=0;
-    } 
-}
 function reset(){
 
 }
+
 function small() {
     grid_div.style.gridTemplateColumns = `repeat(16, 1fr)`
     grid_div.style.gridTemplateRows = `repeat(16, 1fr)`
@@ -129,8 +67,8 @@ function small() {
           const div = document.createElement("div");
           grid_div.appendChild(div);
         }
-
 }
+
 function medium() {
     grid_div.style.gridTemplateColumns = `repeat(32, 1fr)`
     grid_div.style.gridTemplateRows = `repeat(32, 1fr)`
@@ -138,8 +76,8 @@ function medium() {
     for (let i = 0; i < 32 * 32; i++) {
           const div = document.createElement("div");
           grid_div.appendChild(div);
-        }
 
+        }
 }
 function large() {
     grid_div.style.gridTemplateColumns = `repeat(64, 1fr)`
@@ -148,15 +86,37 @@ function large() {
           const div = document.createElement("div");
           grid_div.appendChild(div);
         }
+}
+
+//grid_item.addEventListener("click", color);
+function color(){
 
 }
-function startover(){
-
+function cursor(){
+ 
 
 }
 function animate(){
     Box.style.animation = "eraseEtch 1s ease";
 }
 
-window.onload = () => {medium()}
+window.onload = () => {small()}
+
+document.addEventListener("keyup", function(key) {
+    if (key.code == 'ArrowUp' || key.code == 'KeyW'){
+      key.preventDefault();
+
+    }
+});
+  document.addEventListener("keyup", function(key) {
+    if (key.code == 'Backspace'){
+      key.preventDefault();
+      Clear_button.style.backgroundColor="darkred";
+        animate();
+        setTimeout(() => {
+            Clear_button.style.backgroundColor="#FE3540";
+            Box.style.animation = "reset";
+        }, 1000);
+    }
+  });
 
