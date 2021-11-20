cb109f56accbe0a0bc351b5b290d92d34ba5aa67
Brian Park <brianshockerpark@gmail.com>
2021-11-19 20:21:35 -0800

    Updated Version - 11/19/2021

--- etch.js
+++ etch.js
@@ -3,22 +3,26 @@ const Eraser_button=document.getElementById("a1");
 const Rainbow_button=document.getElementById("a3");
 const Fill_button=document.getElementById("a4");
 const Color_button=document.getElementById("a5");
-const canvas_div=document.getElementById("canvas");
+const grid_div=document.getElementById("grid");
 const Clear_button=document.getElementById("clear");
 const Save_button=document.getElementById("save");
+const Slider=document.getElementById("slider");
+const Dimension=document.getElementById("dimension");
+const Box=document.getElementById("box");
+
+const Default=16;
 let x=0;
 let y=0;
-
 
 function Draw() {
     x++;
     if(x % 2 === 0){
-        Draw_button.style.backgroundColor="orangered";
+        Draw_button.style.backgroundColor= "#FE3540";
         Eraser_button.disabled = false;
         Rainbow_button.disabled = false;
         Fill_button.disabled = false;
         Color_button.disabled = false;
-         x=0; 
+        x=0; 
     } 
     else{
         Draw_button.style.backgroundColor="darkred";
@@ -32,7 +36,7 @@ function Draw() {
 function Eraser() {
     x++;
     if(x % 2 === 0){
-        Eraser_button.style.backgroundColor="orangered";
+        Eraser_button.style.backgroundColor="#FE3540";
         Draw_button.disabled = false;
         Rainbow_button.disabled = false;
         Fill_button.disabled = false;
@@ -50,7 +54,7 @@ function Eraser() {
 function Rainbow() {
     x++;
     if(x % 2 === 0){
-        Rainbow_button.style.backgroundColor="orangered";
+        Rainbow_button.style.backgroundColor="#FE3540";
         Eraser_button.disabled = false;
         Draw_button.disabled = false;
         Fill_button.disabled = false;
@@ -69,7 +73,7 @@ function Rainbow() {
 function Fill() {
     x++;
     if(x % 2 === 0){
-        Fill_button.style.backgroundColor="orangered";
+        Fill_button.style.backgroundColor="#FE3540";
         Eraser_button.disabled = false;
         Rainbow_button.disabled = false;
         Draw_button.disabled = false;
@@ -86,7 +90,7 @@ function Fill() {
 function Color() {
     x++;
     if(x % 2 === 0){
-        Color_button.style.backgroundColor="orangered";
+        Color_button.style.backgroundColor="#FE3540";
         Eraser_button.disabled = false;
         Rainbow_button.disabled = false;
         Fill_button.disabled = false;
@@ -100,18 +104,59 @@ function Color() {
         Draw_button.disabled = true;
     }
 }
-function Save() {
-    x++;
-    if(x % 2 === 0){
-        Save_button.style.backgroundColor="orangered";
-    } 
-    else{
-        Save_button.style.backgroundColor="darkred";
-    }
-}
-function Clear() {
 
+
+function Clear() {
+    y++;
+    if(y === 1){
+        Clear_button.style.backgroundColor="darkred";
+        animate();
+        setTimeout(() => {
+            Clear_button.style.backgroundColor="#FE3540";
+            Box.style.animation = "reset";
+        }, 1000);
+        y=0;
+    } 
 }
 function reset(){
 
 }
+function small() {
+    grid_div.style.gridTemplateColumns = `repeat(16, 1fr)`
+    grid_div.style.gridTemplateRows = `repeat(16, 1fr)`
+    
+    for (let i = 0; i < 16 * 16; i++) {
+          const div = document.createElement("div");
+          grid_div.appendChild(div);
+        }
+
+}
+function medium() {
+    grid_div.style.gridTemplateColumns = `repeat(32, 1fr)`
+    grid_div.style.gridTemplateRows = `repeat(32, 1fr)`
+    
+    for (let i = 0; i < 32 * 32; i++) {
+          const div = document.createElement("div");
+          grid_div.appendChild(div);
+        }
+
+}
+function large() {
+    grid_div.style.gridTemplateColumns = `repeat(64, 1fr)`
+    grid_div.style.gridTemplateRows = `repeat(64, 1fr)`
+    for (let i = 0; i < 64 * 64; i++) {
+          const div = document.createElement("div");
+          grid_div.appendChild(div);
+        }
+
+}
+function startover(){
+
+
+}
+function animate(){
+    Box.style.animation = "eraseEtch 1s ease";
+}
+
+window.onload = () => {medium()}
+
