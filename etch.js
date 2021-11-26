29a340c8288395f2e28d3ff972d0ef8a9f4e109e
Brian P <brianshockerpark@gmail.com>
2021-11-26 09:40:44 -0800

    Update - 11/26/2021

--- etch.js
+++ etch.js
@@ -10,10 +10,15 @@ const Slider=document.getElementById("slider");
 const Dimension=document.getElementById("dimension");
 const Box=document.getElementById("box");
 const Default=16;
-const nameArray= [Draw_button,Eraser_button,Rainbow_button,Fill_button,Color_button];
+const array=[];
+const nameArray=[Draw_button,Eraser_button,Rainbow_button,Fill_button,Color_button];
 let x=0;
-const active=document.getElementsByClassName("pixel");
+const center=((32*32)/2)+15;
+let move=0;
+//const active=document.getElementsByClassName("pixel");
+
 //const currentMode;
+
 Draw_button.addEventListener("click", function() {
     for (let i = 0; i <= 4 ; i++) {
         nameArray[i].style.backgroundColor="#FE3540";
@@ -67,8 +72,8 @@ function small() {
     for (let i = 0; i < 16 * 16; i++) {
           const pixel = document.createElement('div');
           pixel.classList.add('pixel');
-          //pixel.style.backgroundColor = 'whitesmoke';
           grid_div.appendChild(pixel);
+          pixel.addEventListener("click", mooed);
         }
 }
 
@@ -78,12 +83,13 @@ function medium() {
     clear();
     
     for (let i = 0; i < 32 * 32; i++) {
-          const pixel = document.createElement('div');
+          let pixel = document.createElement('div');
           pixel.classList.add('pixel');
-          pixel.id="pixel";
-          //pixel.style.backgroundColor = 'whitesmoke';
+          pixel.id=("pixel"+i);
+          let array=["pixel"+i];
+          //console.log(array[i]);
           grid_div.appendChild(pixel);
-
+          pixel.addEventListener("click", mooed);
         }
 }
 function large() {
@@ -108,8 +114,10 @@ function color(){
 }
 
 function cursor(){
-
-
+    let center=((32*32)/2)+15;
+    console.log(center);
+    document.getElementById("pixel"+center).classList.toggle("active");
+    return center;
 }
 function animate(){
     Box.style.animation = "eraseEtch 1s ease";
@@ -117,13 +125,24 @@ function animate(){
 
 window.onload = () => {small()}
 
-document.addEventListener("keyup", function(key) {
+document.addEventListener("keydown", function(key) {
     if (key.code == 'ArrowUp' || key.code == 'KeyW'){
       key.preventDefault();
-
+        
+        move=center+32;
+        console.log(move);
+        document.getElementById("pixel"+move).classList.add("active");
+        return move;
     }
 });
-  document.addEventListener("keyup", function(key) {
+
+document.addEventListener("keyup", function(key) {
+    if (key.code == 'KeyN'){
+      key.preventDefault();
+        cursor();
+    }
+});
+document.addEventListener("keyup", function(key) {
     if (key.code == 'Backspace'){
       key.preventDefault();
       Clear_button.style.backgroundColor="darkred";
@@ -133,18 +152,14 @@ document.addEventListener("keyup", function(key) {
             Box.style.animation = "reset";
         }, 1000);
     }
-  });
-
-
-grid_div.addEventListener('mousedown', function() {
-    mooed();
-    //grid_div.removeChild(pixel);
-    document.getElementById("pixel").classList.toggle("active");
-
-    //grid_div.style.backgroundColor="black";
 });
 
-
+// let griditems=document.querySelectorAll('pixel.grid_div');
+// for(let i=0;i>griditems.length;i++){
+//     griditems[i].addEventListener("click", mooed);
+// }
  function mooed(){
      console.log("mooing over here");
+     //document.getElementById("pixel").classList.toggle("active");
+
  }
